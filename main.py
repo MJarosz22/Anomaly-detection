ea6ff1bfb641a88a3c62467ab689836b9e09a392
MJarosz22 <99798132+MJarosz22@users.noreply.github.com>
2023-01-05 15:09:21 +0100

    Add comments

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.ensemble import IsolationForest
 from sklearn.metrics import f1_score
 
-
+# Extracts date from a timestamp
 def extract_time(time):
     format = '%d/%m/%Y %I:%M:%S %p'
     if(time == "Timestamp"):
@@ -13,7 +13,7 @@ def extract_time(time):
     date = datetime.strptime(time, format).timestamp()
     return date
 
-
+# loads the data from a csv file
 def load_data(file):
     rows = []
     with open(file, mode='r') as csv_file:
@@ -36,16 +36,17 @@ def create_submission(submission, file):
         writer = csv.writer(csv_file)
         writer.writerows(submission)
 
-
+# load the data
 train_data = np.array(load_data("data/train_data.csv")[:, 1:-1], dtype=float)
 ids = load_data("data/test_data.csv")[:, 0]
 test_data = np.array(load_data("data/test_data.csv")[:, 1:], dtype=float)
 val_data = np.array(load_data("data/val_data.csv")[:, 1:-1], dtype=float)
 
+# train the model
 model = IsolationForest(n_estimators=26, max_samples='auto', contamination=float(0.006))
 model.fit(train_data)
 
-
+# make predictions for each data point
 anomaly = model.predict(test_data)
 anomaly[anomaly == 1] = 0
 anomaly[anomaly == -1] = 1
@@ -54,10 +55,11 @@ predicted = model.predict(val_data)
 predicted[predicted == 1.] = 0.
 predicted[predicted == -1.] = 1.
 
+# calculate f1 score
 actual = np.array(load_data("data/val_data.csv")[:, -1], dtype=float)
 f1 = f1_score(actual, predicted, average='micro')
 print("F1 score: ", f1)
 
+# write the predictions to a csv file
 res = np.vstack((ids, anomaly)).T
 create_submission(res, "submission.csv")
-# the submission has to be reformatted manually!
